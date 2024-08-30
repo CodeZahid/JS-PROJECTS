0871a9f203b67a3dc76213c1487ff9961d158b67
Md Zahid <147043130+CodeZahid@users.noreply.github.com>
2024-08-30 09:31:21 +0530

    This is a clone for google-gemini (AI chatbot)
    It took me around 4 hours to complete, I know it's a long time but this is my very first JavaScritp Project. and it gave me a hard time. but I'll learn in future and become super fast and accurate.

---
+++ Gemini - Google ChatBot/script.js
@@ -0,0 +1,187 @@
+const typingForm = document.querySelector(".typing-form");
+const chatList = document.querySelector(".chat-list");
+const suggestions = document.querySelectorAll(".suggestion-list .suggestion")
+const toggleThemeButton = document.querySelector("#toggle-theme-button");
+const deleteChatButton = document.querySelector("#delete-chat-button");
+
+let userMessage = null;
+let isResponseGenerating = false;
+
+//API configuration
+const API_KEY = `AIzaSyD7I1shiYkz035Pe1r-wgQwt1kPtI207xc`;
+const API_URL = `https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent?key=${API_KEY}`;
+
+
+const loadLocalstorageData = () => {
+    const savedChats = localStorage.getItem("savedChats");
+    const isLightMode = (localStorage.getItem("themeColor") === "light_mode");
+
+    //Apply the stored theme
+    document.body.classList.toggle("light_mode", isLightMode);
+    toggleThemeButton.innerText = isLightMode ? "dark_mode" : "light_mode";
+
+    //Restore saved chats...
+
+    chatList.innerHTML = savedChats || "";
+
+    document.body.classList.toggle("hide-header", savedChats);
+    chatList,scrollTo(0, chatList.scrollHeight); // scroll to the bottom
+}
+
+loadLocalstorageData();
+
+
+//Create a new element and return it...
+const createMessageElement = (content, ...classes) => {
+    const div = document.createElement("div");
+    div.classList.add("message", ...classes);
+    div.innerHTML = content;
+    return div;
+}
+
+
+// showing typing effect by displaying words one by one
+const showtypingEffect = (text, textElement, incomingMessageDiv) => {
+    const words = text.split(' ');
+    let currentWordIndex = 0;
+
+    const typingInterval = setInterval(() => {
+        //Append each word to the text element with a space
+        textElement.innerText += (currentWordIndex === 0 ? '' : ' ') + words[currentWordIndex++];
+        incomingMessageDiv.querySelector(".icon").classList.add("hide");
+
+        //if all words are displayed
+        if(currentWordIndex === words.length) {
+            clearInterval(typingInterval);
+            isResponseGenerating = false;
+            incomingMessageDiv.querySelector(".icon").classList.remove("hide");
+            localStorage.setItem("savedChats", chatList.innerHTML); // save the chat to local storage.
+            chatList,scrollTo(0, chatList.scrollHeight); // scroll to the bottom
+        }
+    }, 75);
+}
+
+// Fetch response from the API based on user messege
+const generateAPIResponse = async (incomingMessageDiv) => {
+    const textElement = incomingMessageDiv.querySelector(".text"); // get text element
+
+    //Send a POST request to the API  with the user's message
+    try {
+        const response = await fetch(API_URL, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                contents: [{
+                    role: "user",
+                    parts: [{ text: userMessage}]
+                }]
+            })
+        });
+
+        const data = await response.json();
+        if(!response.ok) throw new Error(data.error.message);
+        
+        //Get the API response text and remove asterisks form it( keeping it simple.)
+        const apiResponse = data?.candidates[0].content.parts[0].text.replace(/\*\*(.*?)\*\*/g, '$1');
+        showtypingEffect(apiResponse, textElement, incomingMessageDiv);
+    } catch (error) {
+        isResponseGenerating = false;
+        textElement.innerText = error.message;
+        textElement.classList.add("error");
+    } finally {
+        incomingMessageDiv.classList.remove("loading");
+    }
+}
+
+
+
+// Show a loading animation while waiting for the API response
+const showLoadingAnimation = () => {
+    const html = `<div class="message-content">
+                <img src="images/gemini.svg" alt="Gemini Image" class="avatar">
+                <p class="text"></p>
+                <div class="loading-indicator">
+                    <div class="loading-bar"></div>
+                    <div class="loading-bar"></div>
+                    <div class="loading-bar"></div>
+                </div>
+            </div>
+            <span onclick="copyMessage(this)" class="icon material-symbols-rounded">content_copy</span>`;
+
+    const incomingMessageDiv = createMessageElement(html, "incoming", "loading");
+    chatList.appendChild(incomingMessageDiv);
+
+    chatList,scrollTo(0, chatList.scrollHeight); // scroll to the bottom
+    generateAPIResponse(incomingMessageDiv);
+}
+
+
+// copy message text to the clipboaed
+const copyMessage = (copyIcon) => {
+    const messageText = copyIcon.parentElement.querySelector(".text").innerText;
+
+    navigator.clipboard.writeText(messageText);
+    copyIcon.innerText = "done";
+    setTimeout(() => copyIcon.innerText = "content_copy", 1000);// revert icon after a second
+}
+
+
+// handle sending outgoing chat messages
+const handleOutgoingChat = () => {
+    userMessage = typingForm.querySelector(".typing-input").value.trim() || userMessage;
+    if(!userMessage || isResponseGenerating) return; //exit if there is no message
+
+    isResponseGenerating = true;
+
+    const html = `<div class="message-content">
+                <img src="images/USERR.jpg" alt="User Image" class="avatar">
+                <p class="text"></p>
+            </div>`;
+
+    const outgoingMessageDiv = createMessageElement(html, "outgoing");
+    outgoingMessageDiv.querySelector(".text").innerHTML = userMessage;
+    chatList.appendChild(outgoingMessageDiv);
+
+    typingForm.reset(); // clear input field.
+    chatList,scrollTo(0, chatList.scrollHeight); // scroll to the bottom
+    document.body.classList.add("hide-header"); // hide the header once the chat starts...
+    setTimeout(showLoadingAnimation, 500); // show loading animation after a delay
+    
+}
+
+
+// Set userMessage and handle outgoing chat when a suggestion is clicked.
+suggestions.forEach(suggestion => {
+    suggestion.addEventListener("click", () => {
+        userMessage = suggestion.querySelector(".text").innerText;
+        handleOutgoingChat();
+    });
+});
+
+toggleThemeButton.addEventListener("click", () => {
+    const isLightMode = document.body.classList.toggle("light_mode");
+    //Saving selected theme on browser local storage.
+    localStorage.setItem("themeColor", isLightMode ? "light_mode" : "dark_mode");
+    toggleThemeButton.innerText = isLightMode ? "dark_mode" : "light_mode";
+});
+
+
+// Delete all chats form local storage whrn button is clicked...
+deleteChatButton.addEventListener("click", () => {
+    if(confirm("Are you sure, you want to delete all the messages?")) {
+        localStorage.removeItem("savedChats");
+        loadLocalstorageData();
+    }
+});
+
+
+
+
+
+
+// prevent default form submission and handle outgoing chat.
+typingForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    handleOutgoingChat();
+});
